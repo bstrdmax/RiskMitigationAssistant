4143fdab16299590f22c1ad063b4b68fc0bfa31a
Jason Maxwell <max371979@gmail.com>
2025-09-09 10:27:33 -0400

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="text-center py-4 text-sm text-gray-500">
+      <p>Powered by Maxwell Risk Group</p>
+    </footer>
+  );
+};
